5448c2bfe64d5a5ae34c2d33191e79d944d1f1c2
Juliana Ariane Silva Novais <155453513+JuAriane@users.noreply.github.com>
2026-03-31 06:08:52 -0300

    Implement Website Monitoring and Logging Functionality (#3)
    * feat: implement website monitoring and logging functionality
    
    * feat: update README with project overview, features, installation, and contribution guidelines (#2)

--- internal/infra/log.go
+++ internal/infra/log.go
@@ -1,1 +1,30 @@
-package main
+package infra
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func RegisterLog(website string, status bool) {
+	file, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + website + " - online: " + fmt.Sprint(status) + "\n")
+
+	file.Close()
+}
+
+func readLog() {
+	file, err := os.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+		return
+	}
+
+	fmt.Println(string(file))
+}
